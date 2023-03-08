0197ca9771930ea45c62c9eb372ffb6921169321
mdziewa <mdziewa@sii.pl>
2023-03-08 17:44:59 +0100

    web driver manage r impl

--- src/test/java/com/sii/academy/javaweek4_2/TestBase.java
+++ src/test/java/com/sii/academy/javaweek4_2/TestBase.java
@@ -1,6 +1,8 @@
 package com.sii.academy.javaweek4_2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,15 +11,17 @@ import java.util.logging.Logger;
 
 public class TestBase {
 
-    protected static final String CHROMEDRIVER_PATH = "C:\\Users\\mdziewa\\webdriver\\chromedriver.exe";
-    protected static final String CHROME_DRIVER_SYSTEM_PROPERTY = "webdriver.chrome.driver";
     private static final Logger logger = Logger.getAnonymousLogger();
     protected WebDriver webDriver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @BeforeEach
     public void setup() {
 
-        System.setProperty(CHROME_DRIVER_SYSTEM_PROPERTY, CHROMEDRIVER_PATH);
         logger.info("Initializing chrome webdriver");
         webDriver = new ChromeDriver();
     }
